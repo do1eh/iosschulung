5f63d9ef79a480345020ea28d4b438a56a72d55d
Ralf Lüsebrink <r.luesebrink@bsdwest.de>
2021-06-16 12:39:17 +0200

    erster commit

---
+++ iosschulung/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosschulung
+//
+//  Created by Ralf Lüsebrink on 16.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
